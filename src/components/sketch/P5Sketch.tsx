b6854f561954b0514e7e1e661e9dd387b70ec965
rakhatyusupov <seminar.p2p@gmail.com>
2025-04-18 17:54:24 +0300

    fixed: resoulution

--- src/components/sketch/P5Sketch.tsx
+++ src/components/sketch/P5Sketch.tsx
@@ -15,9 +15,7 @@ interface P5SketchProps {
   resolution: Resolution;
 }
 
-const effectsLibrary: {
-  [key: string]: (p: p5, mx: number, my: number) => void;
-} = {
+const effectsLibrary = {
   circle: drawCircle,
   rectangle: drawRect,
 };
@@ -40,12 +38,10 @@ const P5Sketch = ({ activeEffects, onExport, resolution }: P5SketchProps) => {
         const canvas = p.createCanvas(1, 1);
         mainCanvas.current = canvas;
         canvas.parent(sketchRef.current!);
-
         bufferCanvas.current = p.createGraphics(
           resolution.width,
           resolution.height
         );
-
         resizeCanvas();
       };
 
@@ -53,11 +49,19 @@ const P5Sketch = ({ activeEffects, onExport, resolution }: P5SketchProps) => {
         const container = sketchRef.current?.parentElement;
         if (!container || !mainCanvas.current) return;
 
-        const parentWidth = container.clientWidth;
-        const parentHeight = container.clientHeight;
+        const targetAspect = resolution.width / resolution.height;
+        const maxWidth = container.clientWidth * 0.8;
+        const maxHeight = container.clientHeight * 0.8;
 
-        let canvasSize = Math.min(parentWidth, parentHeight) * 0.8;
-        p.resizeCanvas(canvasSize, canvasSize);
+        let canvasWidth = maxWidth;
+        let canvasHeight = canvasWidth / targetAspect;
+
+        if (canvasHeight > maxHeight) {
+          canvasHeight = maxHeight;
+          canvasWidth = canvasHeight * targetAspect;
+        }
+
+        p.resizeCanvas(canvasWidth, canvasHeight);
         mainCanvas.current.style("display", "block");
         mainCanvas.current.style("margin", "auto");
       };
@@ -66,16 +70,18 @@ const P5Sketch = ({ activeEffects, onExport, resolution }: P5SketchProps) => {
         if (!bufferCanvas.current) return;
 
         const buffer = bufferCanvas.current;
-        const scaleFactor = resolution.width / p.width;
-
-        const bufferMouseX = p.mouseX * scaleFactor;
-        const bufferMouseY = p.mouseY * scaleFactor;
+        const scaleX = resolution.width / p.width;
+        const scaleY = resolution.height / p.height;
 
         buffer.background(0);
 
         activeEffectsRef.current.forEach((effect) => {
           if (effectsLibrary[effect]) {
-            effectsLibrary[effect](buffer, bufferMouseX, bufferMouseY);
+            effectsLibrary[effect](
+              buffer,
+              p.mouseX * scaleX,
+              p.mouseY * scaleY
+            );
           }
         });
 
